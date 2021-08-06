e57f8b845ca11841ac271dc3f880642727e68750
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 01:55:13 +0530

    Add files via upload

---
+++ RectangleWithProperties/Model/Rectangle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleWithProperties.Model
+{
+    class Rectangle
+    {
+        private int _height;
+        private int _width;
+        private BorderStyle _border = BorderStyle.SOLID;
+
+        public Rectangle(int width, int height) : this(width,height,BorderStyle.SOLID)
+        { }
+
+        public Rectangle(int width, int height, BorderStyle border)
+        {
+            _width = width;
+            _height = height;
+            _border = border;
+        }
+
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                if (value > 0)
+                    _height = value;
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                if (value > 0)
+                    _width = value;
+            }
+        }
+
+        public BorderStyle Border{
+            get
+            {
+                return _border;
+            }
+            set{
+                _border = BorderStyle.SOLID;
+            }
+        }
+
+        public int calculateArea()
+        {
+            return _width * _height;
+        }
+    }
+}
---
+++ RectangleWithProperties/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RectangleWithProperties.Model;
+
+namespace RectangleWithProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(200, 30);
+            Console.WriteLine("Default Border: "+r1.Border);
+            r1.Border = BorderStyle.DOTTED;
+            Console.WriteLine("New Border: " + r1.Border);
+
+            Console.ReadLine();
+
+        }
+    }
+}
